c04151c1cbf9e72f38257374daa6cefddb0419c0
back-end <felipe.arcanjo@iesb.edu.br>
2024-03-01 21:55:35 -0300

    rota

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const express = require('express')
+const app = express()
 
-console.log('abriu o arquivo')
+app.get('/', function(req, res){
+    res.send('rota princiapal')
+})
+
+app.get('/nome', function(req, res){
+    res.send('Felipe Vieira Arcanjo')
+})
+
+app.get('/hello', function(req, res){
+    res.send('chegou a reposta')
+})
+
+app.listen('3000', function () {
+    console.log('executou');
+})
